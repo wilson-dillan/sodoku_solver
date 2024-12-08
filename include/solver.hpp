a46a3b5b38bfc44791f8735cfa8d528fc0319337
Dillan Wilson <dillanwilson87@gmail.com>
2024-12-07 20:47:56 -0500

    adding include files

--- include/solver.hpp
+++ include/solver.hpp
@@ -14,11 +14,15 @@ class solver {
     solver(unique_ptr<board>); // solver needs a board unique pointer
     unique_ptr<board> solve();
     bool isValidBoard(); // returns true if the solvedBoard_ is valid
-    setBundlePtr getSetsFromRowColCell(int,int);// returns a vector of sets for each row, column and cell. 27
+    set<int> getCandidatesFromCoordinate(int,int); // returns all valid coordinates for curr point
     board& getInitialBoardRef(); // returns reference to initialBoard_ memory
+    bool populateCell(int); // fill the given cell with valid values
+    bool testIfSolutionAndInitialMatch();
+    void printSolvedBoard();
   private:
     unique_ptr<board> initialBoard_;
     unique_ptr<board> solvedBoard_;
+    vector<int> order; // order in which cells to solve
     
     
 }; 
